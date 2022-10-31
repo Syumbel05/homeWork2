c3ab1c491ad8efdc024d745e7394254dd0eb15ac
syumbel <syu.kashapova@mail.ru>
2022-10-31 19:49:30 +0500

    добавила 3 код

---
+++ hw03/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число от 1 до 7");
+int x=Convert.ToInt32(Console.ReadLine());
+if (x==1)Console.WriteLine("нет");
+else if (x==2)Console.WriteLine("нет");
+else if (x==3)Console.WriteLine("нет");
+else if (x==4)Console.WriteLine("нет");
+else if (x==5)Console.WriteLine("нет");
+else if (x==6)Console.WriteLine("ура,выходной");
+else if (x==7)Console.WriteLine("ура,выходной");
